db5e809e7ff875ab157232923d37980e1ea654a0
wendacheng <1125102114@qq.com>
2019-10-11 01:23:05 +0800

    initial commit

---
+++ src/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env/ python3
+# -*- coding: utf-8 -*-
+from src.mainFunc import *
+from src.model import *
+from src.dataProcessing import *
+
+if __name__ == "__main__":
+    pass
--- src/mainFunc.py
+++ src/mainFunc.py
@@ -4,3 +4,17 @@
     1. Strategy implementation
     2. Calculate Sharpe Ratio
 '''
+def strategy(mean, upperBound, lowerBound):
+    '''
+    :param mean: mean value
+    :param upperBound: form a interval [lowerBound, upperBound] together with lowerBound
+    :param lowerBound: form a interval [lowerBound, upperBound] together with upperBound
+    :return: portfolio of option
+    '''
+    return
+
+def getSharpeRatio():
+    '''
+    :return: Sharpe Ratio of an investment
+    '''
+    return
